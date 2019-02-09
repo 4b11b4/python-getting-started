a6540e45f649707954f0b0308a68f4cf52c5db81
AB <4b11b4@gmail.com>
2019-02-09 01:25:39 -0800

    tabs

--- hello/serializers.py
+++ hello/serializers.py
@@ -4,10 +4,10 @@ from .models import Greeting, User
 class GreetingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Greeting
-	fields = ('when', 'name')
+        fields = ('when', 'name')
 
 # What is the difference between a Hyperlinked and regular Model serializer?
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-	fields = ('name')
+        fields = ('name')
